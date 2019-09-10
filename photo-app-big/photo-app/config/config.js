7a06b3fc3e5d2ee7a084fc4098994565ff153a66
Jeff Rosal <jeros808@aol.com>
2019-09-09 21:25:26 -0500

    add remove stuff

--- photo-app-big/photo-app/config/config.js
+++ photo-app-big/photo-app/config/config.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBYwv6f7FMESN2ztHP3JVwCpolGqNCyWbE",
-  authDomain: "photo-feed-46cb7.firebaseapp.com",
-  databaseURL: "https://photo-feed-46cb7.firebaseio.com",
-  projectId: "photo-feed-46cb7",
-  storageBucket: "photo-feed-46cb7.appspot.com",
-  messagingSenderId: "1072425226839",
-  appId: "1:1072425226839:web:d6f83beed8a551fb"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ",
+  appId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
